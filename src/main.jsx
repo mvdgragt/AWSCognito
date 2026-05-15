79b324b742b885d7fdd1e54d814cf54de0f8c870
mvdgragt <12345678+mvdgragt@users.noreply.github.com>
2026-05-15 17:28:31 +0200

    feat: set up TanStack Router with protected routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Amplify } from "aws-amplify";
 import awsConfig from "./aws-config.js";
+import { RouterProvider } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { router } from "./router";
 import "./index.css";
-import App from "./App.jsx";
 
 Amplify.configure(awsConfig);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
